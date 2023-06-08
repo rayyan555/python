5c711422c997ce5532bd2b5a6666e614870591f9
rayyan555 <131010274+rayyan555@users.noreply.github.com>
2023-06-08 11:30:48 +0530

    Add files via upload

---
+++ basics programs/nonstop.py
@@ -0,0 +1,12 @@
+even_count=0
+odd_count=0
+while True:
+    n=int(input(" enter a number"))
+    if n==-1:
+         break
+    if n%2==0:
+         even_count+=1
+    else:
+         odd_count+=1
+print("odd count is",odd_count)
+print("even count is",even_count)           
