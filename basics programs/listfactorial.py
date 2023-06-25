a9b41c82d14037834b4a3c2b590ce5d46b25e33e
rayyan555 <131010274+rayyan555@users.noreply.github.com>
2023-06-25 15:40:51 +0530

    Add files via upload

---
+++ basics programs/listfactorial.py
@@ -0,0 +1,16 @@
+# Read the list of 5 numbers from the user
+numbers = []
+for i in range(5):
+    num = int(input(f"Enter number {i+1}: "))
+    numbers.append(num)
+
+# Calculate the factorial of each number and store them in a new list
+factorials = []
+for num in numbers:
+    factorial = 1
+    for i in range(1, num + 1):
+        factorial *= i
+    factorials.append(factorial)
+
+# Print the resulting list of factorials
+print("Factorials:", factorials)
